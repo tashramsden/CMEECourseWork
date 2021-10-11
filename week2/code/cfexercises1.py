8c38711eccfcd56ac6bc578cdc98d962329a3619
tashramsden <natasha.ramsden18@gmail.com>
2021-10-11 17:39:45 +0100

    First commit of week2 - python work up to variable scope

---
+++ week2/code/cfexercises1.py
@@ -0,0 +1,43 @@
+# finds square root of x (x to the power 0.5)
+def foo_1(x):
+    return x ** 0.5
+
+# returns the bigger number out of the two inputs
+def foo_2(x, y):
+    if x > y:
+        return x
+    return y
+
+# moves the biggest number to the last position (if re-run w output would sort all into ascending order)
+def foo_3(x, y, z):
+    if x > y:
+        # swaps positions depending on relative size
+        tmp = y
+        y = x
+        x = tmp
+    if y > z:
+        tmp = z
+        z = y
+        y = tmp
+    return [x, y, z]
+
+# calculates factorial of x
+def foo_4(x):
+    result = 1
+    for i in range(1, x + 1):
+        result = result * i
+    return result
+
+# a recursive function that calculates the factorial of x
+def foo_5(x):
+    if x == 1:
+        return 1
+    return x * foo_5(x - 1)
+
+# Calculate the factorial of x in a different way
+def foo_6(x):
+    facto = 1
+    while x >= 1:
+        facto = facto * x
+        x = x - 1
+    return facto
---
+++ week2/sandbox/variable_scope.py
@@ -0,0 +1,23 @@
+_a_global = 10 # a global variable
+
+if _a_global >= 5:
+    _b_global = _a_global + 5 # also a global variable
+
+def a_function():
+    _a_global = 5 # a local variable
+
+    if _a_global >= 5:
+        _b_global = _a_global + 5 # also a local variable
+
+    _a_local = 4
+
+    print("Inside the function, the value of _a_global is ", _a_global)
+    print("Inside the function, the value of _b_global is ", _b_global)
+    print("Inside the function, the value of _a_local is ", _a_local)
+
+    return None
+
+a_function()
+
+print("Outside the function, the value of _a_global is ", _a_global)
+print("Outside the function, the value of _b_global is ", _b_global)
