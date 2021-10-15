78405b2c81a0ec52c19c75eca6d96752d503a2de
tashramsden <natasha.ramsden18@gmail.com>
2021-10-15 17:21:36 +0100

    Inputs from working dir, output pdf to results

--- week1/code/CompileLaTeX.sh
+++ week1/code/CompileLaTeX.sh
@@ -5,16 +5,14 @@
 # Arguments: 1 -> The name of a LaTeX .tex file in the working directory (can have .tex extension or none)
 
 
-if [ $# -ne 1 ]; then
-    echo "You need to provide one TeX file to compile from the current directory.";
+if [ $# -ne 1 ] || [ -d $1 ]; then
+    echo "You need to provide the name of one TeX file to compile from the current directory.";
     exit;
 fi
 
 
-#### This method a bit clunky - hard coded paths.... (but does allow compiling from different directory than working dir and outputs pdf to results)
-
-## .TEX AND .BIB IN ../data/LaTeX
-## must be saved in ../data/LaTeX - arg passed is still just name of file not path - FirstExample/FirstExample.tex
+#### This method a bit clunky - hard coded paths.... (but DOES allow compiling from different (set) directory than working dir and outputs pdf to results)
+## .tex and .bib must be saved in ../data/LaTeX - arg passed is still just name of file NOT inc. path - FirstExample or FirstExample.tex
 
 # pdflatex --output-directory ../results ../data/LaTeX/$(basename "$1" .tex).tex
 ## .bib has to be in same location as outputs for biblio to be compiled correctly
@@ -36,13 +34,13 @@ fi
 
 
 ## Alternatively!!
-## Inputs from working directory, output goes to results
+## Inputs from working directory, output goes to results (can pass FirstExample or FirstExample.tex)
 pdflatex $(basename "$1" .tex).tex
 bibtex $(basename "$1" .tex)
 pdflatex $(basename "$1" .tex).tex
 pdflatex $(basename "$1" .tex).tex
 
-# save ouput pdf to results instead
+# save output pdf to results instead
 cp $(basename "$1" .tex).pdf ../results
 rm $(basename "$1" .tex).pdf
 
