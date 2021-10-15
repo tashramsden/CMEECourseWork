6b0f0022af4f1da46c9283ff9b1fc4fe2f0bf9c2
tashramsden <natasha.ramsden18@gmail.com>
2021-10-15 17:30:04 +0100

    One line solutions

--- week2/code/dictionary.py
+++ week2/code/dictionary.py
@@ -19,17 +19,18 @@ taxa = [ ('Myotis lucifugus','Chiroptera'),
 #  OR,
 # 'Chiroptera': {'Myotis lucifugus'} ... etc
 
-# create empty dict with key names
-taxa_dict = {entry[1]: set() for entry in taxa}
+## First attempt:
+## create empty dict with key names
+# taxa_dict = {entry[1]: set() for entry in taxa}
 
-# populate dict w values
-for entry in taxa:
-    taxa_dict[entry[1]].add(entry[0])
+## populate dict w values
+# for entry in taxa:
+#     taxa_dict[entry[1]].add(entry[0])
 
-print(taxa_dict)
+# print(taxa_dict)
 
 # ONE LINE
 taxa_dict2 = {entry[1]: set(row[0] for row in taxa if row[1] == entry[1]) for entry in taxa}
 
-print(f"\nThe same using one line of code!:\n{taxa_dict2}")
+print(taxa_dict2)
 
