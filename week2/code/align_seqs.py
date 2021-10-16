5739067044d047ff8cbb1e2e9a4e395f3f8503e5
tashramsden <natasha.ramsden18@gmail.com>
2021-10-16 17:52:26 +0100

    Make layout clearer

--- week2/code/align_seqs.py
+++ week2/code/align_seqs.py
@@ -4,11 +4,37 @@ __author__ = 'Tash Ramsden (tash.ramsden21@imperial.ac.uk)'
 __version__ = '0.0.1'
 
 ## imports ##
-# import sys
 import csv
 
+seq_csv = "../data/sequences.csv"
+
+## functions ##
+def calculate_score(s1, s2, l1, l2, startpoint):
+    """A function that computes a score by returning the number of matches starting from arbitrary startpoint (chosen by user)"""
+    # import ipdb; ipdb.set_trace()
+    matched = "" # to hold string displaying alignments
+    score = 0
+    for i in range(l2):
+        if (i + startpoint) < l1:
+            if s1[i + startpoint] == s2[i]: # if the bases match
+                matched = matched + "*"
+                score = score + 1
+            else:
+                matched = matched + "-"
+
+    # some formatted output
+    print("." * startpoint + matched)           
+    print("." * startpoint + s2)
+    print(s1)
+    print(score) 
+    print(" ")
+
+    return score
+
+
+## Get sequences and lengths ##
 # read in csv file containing 2 sequences
-with open("../data/sequences.csv", "r") as seq_file:
+with open(seq_csv, "r") as seq_file:
     csvread = csv.reader(seq_file)
     seqs = [seq for seq in csvread]
 
@@ -28,35 +54,14 @@ else:
     s2 = seq1
     l1, l2 = l2, l1 # swap the two lengths
 
-# A function that computes a score by returning the number of matches starting
-# from arbitrary startpoint (chosen by user)
-def calculate_score(s1, s2, l1, l2, startpoint):
-    # import ipdb; ipdb.set_trace()
-    matched = "" # to hold string displaying alignements
-    score = 0
-    for i in range(l2):
-        if (i + startpoint) < l1:
-            if s1[i + startpoint] == s2[i]: # if the bases match
-                matched = matched + "*"
-                score = score + 1
-            else:
-                matched = matched + "-"
-
-    # some formatted output
-    print("." * startpoint + matched)           
-    print("." * startpoint + s2)
-    print(s1)
-    print(score) 
-    print(" ")
-
-    return score
 
 # Test the function with some example starting points:
 # calculate_score(s1, s2, l1, l2, 0)
 # calculate_score(s1, s2, l1, l2, 1)
 # calculate_score(s1, s2, l1, l2, 5)
 
-# now try to find the best match (highest score) for the two sequences
+
+## Find best alignment ##
 my_best_align = None
 my_best_score = -1
 
@@ -69,6 +74,8 @@ print(my_best_align)
 print(s1)
 print("Best score:", my_best_score)
 
+
+## Save best alignemnt information to text file in ../results ##
 output_contents = f"Aligned sequences:\n\n{my_best_align}\n{s1}\n\nBest score: {my_best_score}"
 print(output_contents)
 
