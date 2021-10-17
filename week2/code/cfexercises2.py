500d7dcf02c7512bc5e6c5949943b2dc86a878d2
tashramsden <natasha.ramsden18@gmail.com>
2021-10-17 16:15:26 +0100

    Update - now all sections in functions

--- week2/code/cfexercises2.py
+++ week2/code/cfexercises2.py
@@ -1,25 +1,70 @@
-# print hello 4 times (multiples of 3 (and 0))
-for j in range(12):
-    if j % 3 == 0:
-        print("hello")
+###############################
 
-# prints hello 5 times (for when remainder of /5 or /4 is 3)
-for j in range(15):
-    if j % 5 == 3:
-        print("hello")
-    elif j % 4 == 3:
-        print("hello")
-
-z = 0
-while z != 15:
-    print("hello")
-    z = z + 3
-
-z = 12
-while z < 100:
-    if z == 31:
-        for k in range(7):
+def hello_1(x):
+    for j in range(x):
+        if j % 3 == 0:
             print("hello")
-    elif z == 18:
+    print(" ")
+
+hello_1(12)
+
+###############################
+
+def hello_2(x):
+    for j in range(x):
+        # print(j)
+        if j % 5 == 3:
+            print("hello")
+        elif j % 4 == 3:
+            print("hello")
+    print(" ")
+
+hello_2(12)
+
+################################
+
+def hello_3(x, y):
+    for i in range(x, y):
         print("hello")
-    z = z + 1
+    print(" ")
+
+hello_3(3, 17)
+
+################################
+
+def hello_4(x):
+    while x != 15:
+        print("hello")
+        x = x + 3
+    print(" ")
+
+hello_4(0)
+
+################################
+
+def hello_5(x):
+    while x < 100:
+        if x == 31:
+            for k in range(7):
+                print("hello")
+        elif x == 18:
+            print("hello")
+        x = x + 1
+    print(" ")
+
+hello_5(12)
+
+################################
+
+# WHILE loop with BREAK
+def hello_6(x, y):
+    while x: 
+        print("hello! " + str(y))
+        y += 1
+        if y == 6:
+            break
+    print(" ")
+
+hello_6(True, 0)
+
+################################
