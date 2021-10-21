93e0f150fc2e47c08c6da1d09369d00abe92a06a
tashramsden <natasha.ramsden18@gmail.com>
2021-10-21 19:14:14 +0100

    Vectorized pops in ricker

--- week3/code/Vectorize2.R
+++ week3/code/Vectorize2.R
@@ -1,29 +1,51 @@
 # Runs the stochastic Ricker equation with gaussian fluctuations
 
-rm(list = ls())
+rm(list = ls())  # removes functions already in workspace
 
-stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2,numyears = 100)
-{
+stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, numyears = 100) {
 
-  N <- matrix(NA, numyears, length(p0))  #initialize empty matrix
+    N <- matrix(NA, numyears, length(p0))  #initialize empty matrix
 
-  N[1, ] <- p0
+    N[1, ] <- p0
 
-  for (pop in 1:length(p0)) { #loop through the populations
+    for (pop in 1:length(p0)) {  # loop through the populations
 
-    for (yr in 2:numyears){ #for each pop, loop through the years
+        for (yr in 2:numyears) {  # for each pop, loop through the years
 
-      N[yr, pop] <- N[yr-1, pop] * exp(r * (1 - N[yr - 1, pop] / K) + rnorm(1, 0, sigma)) # add one fluctuation from normal distribution
+            N[yr, pop] <- N[yr-1, pop] * exp(r * (1 - N[yr - 1, pop] / K) + rnorm(1, 0, sigma)) # add one fluctuation from normal distribution
     
-     }
+        }
   
-  }
- return(N)
+    }
+
+    return(N)
 
 }
+
+# print("Non-Vectorized Stochastic Ricker takes:")
+# print(system.time(res1<-stochrick()))
 
 # Now write another function called stochrickvect that vectorizes the above to
 # the extent possible, with improved performance: 
 
-# print("Vectorized Stochastic Ricker takes:")
-# print(system.time(res2<-stochrickvect()))
+stochrickvect <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, numyears = 100) {
+
+    N <- matrix(NA, numyears, length(p0))  # initialize empty matrix
+
+    N[1, ] <- p0
+
+    # cannot vectorize rows (years) as calculations depend on previous year (not independent)
+    for (yr in 2:numyears){  # for each pop, loop through the years (rows)
+
+        # pop can be vectorized as all pops independent of each other
+        # N[yr,] selects whole column (population)
+        N[yr, ] <- N[yr-1, ] * exp(r * (1 - N[yr - 1, ] / K) + rnorm(1, 0, sigma))  # add one fluctuation from normal distribution
+    
+    }
+   
+    return(N)
+
+}
+
+print("Vectorized Stochastic Ricker takes:")
+print(system.time(res2<-stochrickvect()))
