f5d075f8f1cbc2e231343fb6fb941651c85b602c
tashramsden <natasha.ramsden18@gmail.com>
2021-10-21 19:00:29 +0100

    Comment

--- week3/code/Ricker.R
+++ week3/code/Ricker.R
@@ -7,7 +7,7 @@ Ricker <- function(N0=1, r=1, K=10, generations=50) {
 
     N[1] <- N0
     for (t in 2:generations) {
-        N[t] <- N[t-1] * exp(r*(1.0-(N[t-1]/K)))
+        N[t] <- N[t-1] * exp(r*(1.0-(N[t-1]/K)))  # DIFFERENCE model - time steps
     }
     return (N)
 
