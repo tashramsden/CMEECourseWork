a46810326fcaff97f9e5ff118cb5714dc3e5332e
tashramsden <natasha.ramsden18@gmail.com>
2021-10-22 09:17:49 +0100

    add defualts to functions

--- week2/code/cfexercises1.py
+++ week2/code/cfexercises1.py
@@ -8,18 +8,18 @@ __version__ = '0.0.1'
 import sys
 
 ## functions ##
-def foo_1(x):
+def foo_1(x=9):
     """Finds square root of a number, x"""
     return f"\nThe square root of {x} is {x ** 0.5}"
 
-def foo_2(x, y):
+def foo_2(x=5, y=1.2):
     """Return the largest of two numbers"""
     if x > y:
         return f"\nThe bigger number of {x} and {y} is: {x}"
     return f"\nThe bigger number of {x} and {y} is: {y}"
 
 # moves the biggest number to the last position (if re-run w output would sort all into ascending order)
-def foo_3(x, y, z):
+def foo_3(x=8, y=9, z=3):
     """Moves the largest of 3 numbers to the last position"""
     if x > y:
         # swaps positions depending on relative size
@@ -32,21 +32,21 @@ def foo_3(x, y, z):
         y = tmp
     return f"\nReordered list: {[x, y, z]}"
 
-def foo_4(x):
+def foo_4(x=3):
     "Returns the factorial of a number, x"
     result = 1
     for i in range(1, x + 1):
         result = result * i
     return f"\nThe factorial of {x} is {result}"
 
-def foo_5(x):
+def foo_5(x=3):
     """Returns the factorial of a number, x, using a recursive method"""
     if x == 1:
         print("\nThe factorial is:")
         return 1
     return x * foo_5(x - 1)
 
-def foo_6(x):
+def foo_6(x=3):
     """Returns the factorial of a number, x"""
     facto = 1
     while x >= 1:
