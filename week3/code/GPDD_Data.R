f692bf9a2ffd07c5d20b2a00f520898292de16aa
tashramsden <natasha.ramsden18@gmail.com>
2021-10-24 19:14:24 +0100

    Map species around the world

---
+++ week3/code/GPDD_Data.R
@@ -0,0 +1,23 @@
+## Mapping the Global Population Dynamics Database (GPDD)
+# a freely available database developed at Silwood
+# Living Planet Index based on this!
+
+# maps package
+require(maps)
+require(tidyverse)
+
+# load GPDD Rdata
+load("../data/GPDDFiltered.RData")
+head(gpdd)
+dplyr::glimpse(gpdd)
+
+# create world map
+map(database = "world", fill=TRUE, col="lightgrey", border="darkgray",
+    bg = "lightblue", xlim = c(-180, 180), ylim = c(-90, 90), wrap=c(-180,180))
+
+# add species from gpdd
+points(x = gpdd$long, y = gpdd$lat, col=alpha("darkgreen", 0.6), lwd=2)
+
+## There is a strong sampling bias towards the UK and North America 
+# (especially the West coast). The rest of the world is largely unsampled; 
+# these data should not be considered to be representative of the whole world.
