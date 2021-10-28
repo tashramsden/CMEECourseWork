4db5b7d5c69670f4fe3606c84cb18445d956c10c
tashramsden <natasha.ramsden18@gmail.com>
2021-10-28 18:52:21 +0100

    Data wrangling tidy prac

--- week3/code/DataWrang.R
+++ week3/code/DataWrang.R
@@ -7,7 +7,8 @@
 MyData <- as.matrix(read.csv("../data/PoundHillData.csv", header = FALSE))
 
 # header = true because we do have metadata headers
-MyMetaData <- read.csv("../data/PoundHillMetaData.csv", header = TRUE, sep = ";")
+MyMetaData <- read.csv("../data/PoundHillMetaData.csv", header = TRUE, 
+                       sep = ";")
 
 ############# Inspect the dataset ###############
 head(MyData)
@@ -27,7 +28,10 @@ dim(MyData)
 # colnames(MyData)  # atm the headers are just row 1 - not treated as headers
 
 ############# Convert raw matrix to data frame ###############
-TempData <- as.data.frame(MyData[-1,], stringsAsFactors = F) #stringsAsFactors = F is important! (maybe not from R 4.0 onwards...)  # This creates dataframe w just data, not col names (first row)
+TempData <- as.data.frame(MyData[-1,], stringsAsFactors = F) #stringsAsFactors 
+# = F is important! (maybe not from R 4.0 onwards...)  # This creates dataframe 
+# w just data, not col names (first row)
+
 # head(TempData)
 colnames(TempData) <- MyData[1,] # assign column names from original data
 # head(TempData)
@@ -37,11 +41,14 @@ rownames(TempData) <- NULL
 # head(TempData)
 
 ############# Convert from wide to long format  ###############
-require(reshape2) # load the reshape2 package - require() similar to library but returns FALSE rather than error if package doesn't exist
+require(reshape2) # load the reshape2 package - require() similar to library 
+# but returns FALSE rather than error if package doesn't exist
 
 ?melt #check out the melt function
 
-MyWrangledData <- melt(TempData, id=c("Cultivation", "Block", "Plot", "Quadrat"), variable.name = "Species", value.name = "Count")
+MyWrangledData <- melt(TempData, 
+                       id=c("Cultivation", "Block", "Plot", "Quadrat"),
+                       variable.name = "Species", value.name = "Count")
 # head(MyWrangledData); tail(MyWrangledData)
 
 ## Now assign correct data types to each column
@@ -53,15 +60,17 @@ MyWrangledData[, "Quadrat"] <- as.factor(MyWrangledData[, "Quadrat"])
 # Int - count - num of species
 MyWrangledData[, "Count"] <- as.integer(MyWrangledData[, "Count"])
 
-str(MyWrangledData)  # 41 diff tree spp, 5/6 quadrats per plot, 12 plots, 4 blocks, over 3 "cultivation" treatments
+str(MyWrangledData)
 head(MyWrangledData)
 dim(MyWrangledData)
 
 ############# Exploring the data (extend the script below)  ###############
 
 require(tidyverse)
-## shows conflicts - e.g. dplyr filter() - beacuse already func called filter, to use dplyr one use dplyr::filter()
-tidyverse_packages(include_self = TRUE)  # the include_self = TRUE means list "tidyverse" as well 
+## shows conflicts - e.g. dplyr filter() - beacuse already func called filter,
+# to use dplyr one use dplyr::filter()
+tidyverse_packages(include_self = TRUE)  # the include_self = TRUE means 
+# list "tidyverse" as well 
 
 # convert dataframe to tibble
 tibble::as_tibble(MyWrangledData)
@@ -72,4 +81,3 @@ dplyr::glimpse(MyWrangledData)  # like str(), but nicer!!!
 dplyr::filter(MyWrangledData, Count>100) #like subset(), but nicer!
 
 dplyr::slice(MyWrangledData, 10:15) # Look at an arbitrary set of data rows
-
---
+++ week3/code/DataWrangTidy.R
@@ -0,0 +1,71 @@
+################################################################
+################## Wrangling the Pound Hill Dataset ############
+################################################################
+# rm(list = ls())
+
+require(tidyverse)
+
+############# Load the dataset ###############
+# header = false because the raw data don't have real headers
+MyData <- as.matrix(read.csv("../data/PoundHillData.csv", header = FALSE))
+
+# header = true because we do have metadata headers
+MyMetaData <- read.csv("../data/PoundHillMetaData.csv", header = TRUE, 
+                       sep = ";")
+
+############# Inspect the dataset ###############
+dplyr::glimpse(MyData)
+utils::View(MyData) #same as fix()
+head(MyData)
+dim(MyData)
+dplyr::glimpse(MyMetaData)
+utils::View(MyMetaData)
+
+############# Replace species absences with zeros ###############
+MyData <- na_if(MyData, "") %>% replace_na(0)
+
+############# Transpose and convert to data frame ###############
+MyData <- data.frame(t(MyData))
+dim(MyData)
+class(MyData)
+
+############# Assign headers and remove row names ###############
+MyData <- MyData %>% 
+    set_names(slice_head(MyData))
+MyData <- slice(MyData, -1)
+
+rownames(MyData) <- NULL
+
+############# Convert from wide to long format  ###############
+## gather() from tidyr, depreciating...
+# MyWrangledData <- tidyr::gather(MyData, key=Species, value=Count, 
+#                                 -c(Cultivation, Block, Plot, Quadrat))
+## more recent:
+MyWrangledData <- pivot_longer(MyData,
+                               # all columns apart from these are species:
+                               cols = -c(Cultivation, Block, Plot, Quadrat),
+                               names_to = "Species",
+                               values_to = "Count")
+
+############# Assign correct data types to each variable ###############
+MyWrangledData <- MyWrangledData %>% 
+    # species as factor too
+    mutate(across(c(Cultivation, Block, Plot, Quadrat, Species), factor))
+MyWrangledData$Count <- as.integer(MyWrangledData$Count)
+
+# length(levels(MyWrangledData$Species))  # 41 different species
+glimpse(MyWrangledData)
+head(MyWrangledData); tail(MyWrangledData)
+dim(MyWrangledData)
+utils::View(MyWrangledData)
+
+############# Exploring the data (extend the script below)  ###############
+tidyverse_packages(include_self = TRUE)
+
+tibble::as_tibble(MyWrangledData)  # convert dataframe to tibble
+
+dplyr::glimpse(MyWrangledData)  # like str(), but nicer!!!
+
+dplyr::filter(MyWrangledData, Count>100) #like subset(), but nicer!
+
+dplyr::slice(MyWrangledData, 10:15) # Look at an arbitrary set of data rows
