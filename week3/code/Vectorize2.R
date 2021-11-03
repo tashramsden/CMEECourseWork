1ff5fcbcc0e89e8a92a8615aedf3eead507fd1c8
tashramsden <natasha.ramsden18@gmail.com>
2021-11-03 17:24:19 +0000

    Add fluctuation to EACH value in vectorized

--- week3/code/Vectorize2.R
+++ week3/code/Vectorize2.R
@@ -2,9 +2,10 @@
 
 rm(list = ls())  # removes functions already in workspace
 
-# set.seed(12345)
+# set.seed(12345)  # set seed and view res for both models to check same output
 
-stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, numyears = 100) {
+stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, 
+                      sigma = 0.2, numyears = 100) {  # sigma is st dev
 
     N <- matrix(NA, numyears, length(p0))  #initialize empty matrix
 
@@ -24,27 +25,36 @@ stochrick <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, nu
 
 }
 
-# print("Non-Vectorized Stochastic Ricker takes:")
-# print(system.time(res1<-stochrick()))
+print("Non-Vectorized Stochastic Ricker takes:")
+print(system.time(res1<-stochrick()))
+# View(res1)
+
 
 # Now write another function called stochrickvect that vectorizes the above to
 # the extent possible, with improved performance: 
 
 # set.seed(12345)
 
-stochrickvect <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2, numyears = 100) {
+stochrickvect <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, 
+                          sigma = 0.2, numyears = 100) {
 
     N <- matrix(NA, numyears, length(p0))  # initialize empty matrix
 
     N[1, ] <- p0
+    
+    # generate matrix of random fluctuations to be added to EACH N value 
+    matrix_size <- length(p0) * (numyears - 1)
+    random_flucts <- matrix(rnorm(matrix_size, 0, sigma), nrow = numyears-1,
+                            ncol = length(p0))
 
     # cannot vectorize rows (years) as calculations depend on previous year (not independent)
-    for (yr in 2:numyears){  # for each pop, loop through the years (rows)
+    for (yr in 2:numyears) {  # for each pop, loop through the years (rows)
 
         # pop can be vectorized as all pops independent of each other
         # N[yr,] selects whole column (population)
-        N[yr, ] <- N[yr-1, ] * exp(r * (1 - N[yr - 1, ] / K) + rnorm(1, 0, sigma))  # add one fluctuation from normal distribution
-    
+        # NOT THIS: N[yr, ] <- N[yr-1, ] * exp(r * (1 - N[yr - 1, ] / K) + rnorm(1, 0, sigma))  # this adds the same value to every pop
+        N[yr, ] <- N[yr-1, ] * exp(r * (1 - N[yr - 1, ] / K) + random_flucts[yr-1,])  # add one fluctuation TO EACH VALUE
+      
     }
 
     return(N)
@@ -53,9 +63,4 @@ stochrickvect <- function(p0 = runif(1000, .5, 1.5), r = 1.2, K = 1, sigma = 0.2
 
 print("Vectorized Stochastic Ricker takes:")
 print(system.time(res2<-stochrickvect()))
-
-# if (res1 == res2) {
-#     print("Yay")
-# } else {
-#     print("no")
-# }
+# View(res2)  # view res1 and res2 - are exactly the same when seed set
